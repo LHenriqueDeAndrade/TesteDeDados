e2884e86712452fb2b2ebbea9735442fcfb48238
LHenriqueDeAndrade <henriquesilvahenrique-2014@outlook.com>
2022-06-09 05:00:42 -0300

    iniciado

---
+++ script.js
@@ -0,0 +1,11 @@
+let Name = "Luiz"
+let Number = 23
+let Year = 2022
+let sub = Year - Number
+console.log(sub)
+let sum = Year + Number
+console.log(sum)
+let message = "Estou mexendo no console.log"
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
